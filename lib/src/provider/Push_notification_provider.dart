68a501399a2c6c10830c196b38e19d585330adbd
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-11-05 16:06:31 -0400

    config

--- lib/src/provider/Push_notification_provider.dart
+++ lib/src/provider/Push_notification_provider.dart
@@ -9,6 +9,24 @@ class PushNotificationProvider{
       print("=========== FCM Token ============");
       print(value);
     });
+
+
+    _firebaseMessaging.configure(
+      onMessage: (info){
+        print("========= on mesage ========");
+        print(info);
+      },
+      onLaunch: (info){
+        print("========= on onLaunch ========");
+        print(info);
+      },
+      onResume: (info){
+        print("========= on onResume ========");
+        print(info);
+      }
+    );
   }
 
 }
+
+//dc98Su96R12xiY5WzMXr_S:APA91bE7-VBW8W1JblsbF5ER5ZMV6iNjt4HOTa1yheoQEX7GPDWUHcD_M6sPCoDkVeq5Qhnb3f7vlICljE5Me7Zo7SeDHFSdZewmbURKyE-NEog7TAfuIN0csDxkA-sAhpLKNufVkTSh
